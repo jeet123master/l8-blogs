6351fc90d2c97b622ae56468d301eefafb63f803
jeet123master <jeetlimboo21@gmail.com>
2021-02-14 20:21:04 +0545

    admin layouts

---
+++ resources/views/layouts/frontend/app.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title') {{ config('app.name', 'Laravel') }}</title>
+
+   
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/theme.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/camera.css') }}">
+    
+</head>
+<body>
+   
+@include('layouts.frontend.includes.header')
+     
+@yield('content')    
+
+@include('layouts.frontend.includes.footer')
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+<style type="text/css">
+	
+	img.slider-img{
+		height: 400px !important
+	}
+	.custom-product{
+		height: 600px;
+	}
+	.slider-text{
+		color: black;
+	}
+	.trending-img{
+		height: 100px;
+	}
+	.trending-item{
+		float: left;
+		width: 20%;	
+	}
+	.trending-wrapper{
+		margin: 30px;
+	}
+</style>
+
+
+</html>
